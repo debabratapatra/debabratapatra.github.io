3d40618d7dbd71a2f42c17cad4e666bffb9b93fd
debabratapatra <debabratapatra12@gmail.com>
2019-05-18 22:38:41 +0530

    Design angular page.

---
+++ js/angular.js
@@ -0,0 +1,41 @@
+window.addEventListener("load",function(event) {
+    angular_module.fetchNgTreeGridCount();
+    angular_module.fetchAngularTreeGridCount();
+});
+
+var angular_module = function() {
+
+    function fetchNgTreeGridCount() {
+        var currentDate = Utility.getCurrentDate(),
+            url = "https://npm-stat.com/downloads/range/2019-02-11:" + currentDate + "/ngtreegrid";
+
+        Utility.ajax(url, function (response) {
+            var total = 0,
+                jData = JSON.parse(response);
+            jData.downloads.forEach(obj => {
+                total += obj.downloads;
+            });
+
+            document.getElementById('ngtreegrid_download_count').innerHTML = total;
+        });
+    }
+
+    function fetchAngularTreeGridCount() {
+        var currentDate = Utility.getCurrentDate(),
+            url = "https://npm-stat.com/downloads/range/2019-04-22:" + currentDate + "/angular-tree-grid";
+
+        Utility.ajax(url, function (response) {
+            var total = 0,
+                jData = JSON.parse(response);
+            jData.downloads.forEach(obj => {
+                total += obj.downloads;
+            });
+
+            document.getElementById('angulartreegrid_download_count').innerHTML = total;
+        });
+    }
+    return {
+        fetchNgTreeGridCount: fetchNgTreeGridCount,
+        fetchAngularTreeGridCount: fetchAngularTreeGridCount
+    };
+}();
